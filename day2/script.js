54e0d17dfc8baadba50efba028e068255017ef59
Anuj Rana <rana1452005@gmail.com>
2026-02-08 22:27:33 +0530

    day2 leraning

---
+++ day2/script.js
@@ -0,0 +1,18 @@
+//Node.js Basics:
+//Intro to Node.js
+//Installing Node.js and npm
+//working with modules
+//file syatem operations
+//understanding HTTP module
+
+
+/*
+js se backend nhi ban sakta
+kyuki js ke pass wo functionality nhi hai jo backend development ke liye chahiye hoti hai
+ryan dhal isne socha js se backend banana chahiye
+gogle chrome ka v8 engine ka code open source hai and ryan dahl us code mein chedkhani kar raha 
+hai 
+fir usne socha ki hame to js me code krna hai 
+kyuki chrome v8 engine bna h c++ mein 
+
+*/
