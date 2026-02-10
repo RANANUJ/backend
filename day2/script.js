a2c7d087de5ffa560a403f34091a3627f7dd7222
Anuj Rana <rana1452005@gmail.com>
2026-02-10 21:27:03 +0530

    file

--- day2/script.js
+++ day2/script.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 
+//....................writeFile.........................
+/*
 fs.writeFile('output.txt', 'Hello, World!', (err) => {
     if (err) {
         console.error(err);
@@ -7,3 +9,21 @@ fs.writeFile('output.txt', 'Hello, World!', (err) => {
         console.log('done');
     }
 });
+*/
+//....................appendFile.........................
+/*fs.appendFile('output.txt', ' Appended text.', function(err) {
+    if (err) 
+        console.error(err);
+     else 
+        console.log('done');
+});
+*/
+
+//....................rename.........................
+
+fs.rename('output.txt', 'newOutput.txt', function(err){
+    if (err)
+        console.error(err);
+        else
+        console.log('done');
+})
