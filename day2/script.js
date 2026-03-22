6d7f7ab7570b1f21cbcbbe3f0a52775eaa291820
Anuj Rana <rana1452005@gmail.com>
2026-03-22 13:38:26 +0530

    .

--- day2/script.js
+++ day2/script.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+//const fs = require('fs');
 
 //....................writeFile.........................
 /*
@@ -20,10 +20,19 @@ fs.writeFile('output.txt', 'Hello, World!', (err) => {
 */
 
 //....................rename.........................
-
+/*
 fs.rename('output.txt', 'newOutput.txt', function(err){
     if (err)
         console.error(err);
         else
         console.log('done');
 })
+        */
+const http = require('http');
+
+
+const server =http.createServer(function(req, res){
+     res.end("Hello World");
+})
+server.listen(3000);
+
---
+++ day3/script1.js
@@ -0,0 +1,10 @@
+//NPM Understnading
+
+//npm - node packages LockManager
+
+
+//installing and uninstalling anything basics and advanced
+//understanding node_modules
+//dependencies
+//devdependencies
+//scripts - understanding default scripts PATH and custom scripts 
